98d7ed56d2c597cc023ef20daea5b3cfe04cf0f4
Steve Yu <soundedrain@gmail.com>
2020-11-12 20:53:28 +0800

    [modify] player image applied

--- components/PlayerCard.vue
+++ components/PlayerCard.vue
@@ -1,9 +1,12 @@
 <template>
-  <nuxt-link :to="`/players/${player.id}`" class="flex flex-row sm:flex-col items-center sm:items-start w-1/2 sm:w-1/3 md:w-1/5 p-4 no-underline text-black" activeClass="text-darker-indigo">
-    <img :src="getAvatar(player.id)" alt="book-01" class="w-1/3 sm:w-full transition-normal hover:brighter hover:translate-y-1 hover:border-indigo">
-    <div class="ml-3 sm:ml-0 w-2/3 sm:w-full sm:w-full sm:flex sm:justify-center sm:flex-wrap">
-      <p class="text-sm my-2 font-bold text-left sm:text-center sm:flex-none sm:w-full">{{ player.name }}</p>
-      <label class="inline-block sm:flex-none rounded-full bg-indigo-light text-white px-2 py-1/2 text-xs">{{ player.team.name || '' }}</label>
+  <nuxt-link :to="`/players/${player.id}`" class="flex flex-row sm:flex-col items-center sm:items-start w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 md:mt-4 p-6 no-underline text-black" activeClass="text-darker-indigo">
+    <div class="w-1/2 sm:w-full relative pb-11/12">
+      <img class="absolute h-full w-full object-cover hover:brighter hover:translate-y-1" :src="player.image && player.image[0].url || getAvatar(player.id)" :alt="player.name">
+    </div>
+    <div class="w-1/2 sm:w-full sm:flex sm:justify-center sm:flex-wrap text-center">
+      <p class="text-sm my-2 font-bold sm:flex-none sm:w-full">{{ player.name }}</p>
+      <label v-if="player.draftPool === 'yes'" class="inline-block sm:flex-none rounded-full bg-indigo-light text-white px-2 py-1 text-xs">{{ player.team.name || '未分隊' }}</label>
+      <label v-else class="inline-block sm:flex-none rounded-full bg-grey-darker text-white px-2 py-1 text-xs">自由球員</label>
     </div>
   </nuxt-link>
 </template>
--- pages/players/_id.vue
+++ pages/players/_id.vue
@@ -11,8 +11,8 @@
     <div class="px-6 md:px-0 mt-4 md:flex flex-wrap order-1 md:-order-1 md:shadow-md">
       <div class="w-full p-8 md:rounded-r-none bg-white shadow-md md:shadow-none">
         <div class="flex flex-wrap">
-          <div class="w-full sm:w-1/4">
-            <img :src="getAvatar(thisPlayer.id)" alt="book-01" class="w-full transition-normal hover:brighter hover:translate-y-1 hover:border-indigo">
+          <div class="w-full sm:w-1/3 relative">
+            <img class="w-full object-fit hover:brighter hover:translate-y-1" :src="thisPlayer.image && thisPlayer.image[0].url || getAvatar(thisPlayer.id)" :alt="thisPlayer.name">
           </div>
           <div class="flex flex-wrap w-full sm:flex-1 sm:pl-8">
             <div class="w-1/2 mt-4 sm:mt-0">
@@ -24,11 +24,11 @@
             <div class="w-1/2 mt-4 sm:mt-0">
               <p class="text-indigo text-sm p-2">擅長位置</p>
               <div class="font-medium text-sm pl-2">
-                <label for="" v-for="p in thisPlayer.position" class="inline-block rounded-full bg-indigo text-white px-2 py-1/2 text-xs mr-2">{{ p }}</label>
+                <label for="" v-for="p in thisPlayer.position" class="inline-block rounded-full bg-indigo text-white px-2 py-1 text-xs mr-2">{{ p }}</label>
               </div>
             </div>
             <div class="w-full sm:w-1/2 mt-4 sm:mt-0">
-              <template v-if="thisPlayer.draftPool">
+              <template v-if="thisPlayer.draftPool === 'yes'">
                 <p class="text-indigo text-sm p-2">Draft Status</p>
                 <div class="font-medium text-sm pl-2">
                   1st Round 1st Pick
