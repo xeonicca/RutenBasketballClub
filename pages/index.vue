3d421648fe7f2c2899414e89675c4312b4d96bd7
Steve Yu <xeonicca@staff.ruten.com.tw>
2020-11-13 14:58:55 +0800

    [modify layout update

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,22 @@
 <template>
   <!-- Content -->
-  <div class="flex flex-1 flex-col md:px-6 pt-10" id="content">
-
-    <!-- Title -->
-    <div class="px-6 md:px-0 flex justify-between items-center -order-1">
+  <div class="flex flex-1 flex-col md:px-6 pt-4" id="content">
+    <div class="px-6 md:px-0 flex items-baseline justify-between border-b-2 border-grey-light mt-2">
       <div>
-        <h2 class="font-bold">公告</h2>
+        <div class="inline-block no-underline border-indigo pb-2 px-4 text-sm mr-2 text-indigo-darkest hover:cursor-pointer js-tab relative active font-bold" data-tab="section-stats">公告</div>
+        <nuxt-link class="no-underline md:hidden inline-block border-indigo pb-2 px-4 text-sm mr-2 text-indigo-darkest hover:cursor-pointer js-tab relative" to="/teams">隊伍</nuxt-link>
+
+        <nuxt-link class="no-underline md:hidden inline-block border-indigo pb-2 px-4 text-sm mr-2 text-indigo-darkest hover:cursor-pointer js-tab relative" to="/players">成員</nuxt-link>
       </div>
     </div>
 
-    <div class="p-12 mt-4 md:flex flex-wrap order-1 md:-order-1 md:shadow-md lh-2 l bg-white">
+    <div class="p-12 mt-4 md:flex flex-wrap md:shadow-md lh-2 l bg-white">
       <div class="flex flex-row sm:flex-col items-center sm:items-start w-full">
         <nuxt-content class="prose prose-sm" :document="announcement" />
       </div>
     </div>
 
-    <div class="p-12 mt-4 md:flex flex-wrap order-1 md:-order-1 md:shadow-md lh-2 l bg-white">
+    <div class="p-12 mt-4 md:flex flex-wrap md:shadow-md lh-2 l bg-white">
       <div class="flex flex-row sm:flex-col items-center sm:items-start w-full">
         <nuxt-content class="prose prose-sm" :document="rules" />
       </div>
