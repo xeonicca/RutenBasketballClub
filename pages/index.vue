7ec3ea8c54bd5b79751822b89d1a378116158123
Steve Yu <xeonicca@staff.ruten.com.tw>
2020-11-20 18:13:37 +0800

    [modify] added hero image

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- Content -->
   <div class="flex-1 md:px-6 pt-4" id="content">
-    <div class="px-6 md:px-0 flex items-baseline justify-between border-b-2 border-grey-light mt-2">
+    <div class="px-6 md:px-0 flex flex-wrap items-baseline justify-between border-b-2 border-grey-light mt-2">
+      <div class="w-full flex-none mb-4"><img class="block w-full" src="/images/hero.jpg" alt=""></div>
       <h4 class="hidden md:inline-block text-grey-dark font-normal">Welcome</h4>
       <div>
         <div class="inline-block no-underline border-indigo pb-2 px-4 text-sm mr-2 text-indigo-darkest hover:cursor-pointer js-tab relative active font-bold" data-tab="section-stats">公告</div>
@@ -19,13 +20,13 @@
     <section>
       <div class="p-8 sm:p-12 m-4 sm:mx-0 md:shadow-md lh-2 bg-white">
         <div class="flex flex-row sm:flex-col items-center sm:items-start w-full">
-          <nuxt-content class="prose prose-sm" :document="draft" />
+          <nuxt-content class="prose prose-sm" :document="rules" />
         </div>
       </div>
 
       <div class="p-8 sm:p-12 m-4 sm:mx-0 md:shadow-md lh-2 bg-white">
         <div class="flex flex-row sm:flex-col items-center sm:items-start w-full">
-          <nuxt-content class="prose prose-sm" :document="rules" />
+          <nuxt-content class="prose prose-sm" :document="draft" />
         </div>
       </div>
     </section>
