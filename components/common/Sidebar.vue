713817d34f018610b0173f6c9f879633be905f98
Steve Yu <soundedrain@gmail.com>
2020-11-21 15:52:22 +0800

    [modify] add game schedule generator

--- components/common/Sidebar.vue
+++ components/common/Sidebar.vue
@@ -26,6 +26,11 @@
           </li>
         </ul>
       </li>
+      <li class="ml-2 mb-4 flex">
+        <img src="/images/iconmonstr-calendar-2.svg" alt="wishlist-icon" class="w-4 h-4 mr-2">
+        <nuxt-link class="text-sm hover:cursor-pointer text-indigo-darkest no-underline font-bold mobile-home-trigger" to="/gameForm" activeClass="text-indigo-dark">每日賽程表產生器</nuxt-link>
+      </li>
+
       <!-- <li class="ml-2 mb-4 flex lg:hidden">
         <img src="/images/profile-default.svg" alt="profile-icon" class="w-4 h-4 mr-2">
         <div class="hover:cursor-pointer text-white lg:text-indigo-darkest no-underline font-medium" id="mobile-profile-trigger">Profile</div>
--- pages/gameForm.vue
+++ pages/gameForm.vue
@@ -1,10 +1,58 @@
 <template>
-<div class="flex flex-1 flex-col md:px-6 pt-10" id="content">
-
+<div class="flex flex-1 flex-wrap md:px-6 pt-10" id="content">
+  <div class="w-full md:w-1/2 px-6 pt-6 md:-order-1">
+    <div class="flex items-center" v-for="team, i in teams">
+      <div class="" style="width: 50px">
+        <vs-avatar dark :key="team.id" class="my-2">
+          <template #text>
+            {{ team.shortName }}
+          </template>
+        </vs-avatar>
+      </div>
+      <div class="flex flex-1 px-4">
+        <label v-for="x in 5" class="inline-flex items-center ml-2">
+          <input type="radio" class="form-radio h-5 w-5 text-indigo-600" @input="checkDuplicate(x, i)" :name="`${team.id}`" v-model="selected[i]" :value="x"><span class="px-1 text-gray-700">{{x}}</span>
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="w-full md:w-1/2 px-4 -order-1">
+    <div class="p-6 bg-white">
+      <table class="table-auto w-full">
+        <thead class="p-2 text-sm leading-loose border-b text-indigo">
+          <tr>
+            <th>Home</th>
+            <th>Away</th>
+          </tr>
+        </thead>
+        <tbody class="p-2">
+          <tr v-for="(tr, i) in teamMatchups" class="border-b">
+            <td class="leading-none py-2">
+              <vs-button class="inline-block"
+                circle
+                icon
+                primary
+              >{{ tr[0].teamSetup }}</vs-button>
+              <span class="ml-2">{{ tr[0].team && tr[0].team.shortName }}</span>
+            </td>
+            <td class="py-2 font-medium">
+              <vs-button class="inline-block"
+                circle
+                icon
+                danger
+              >{{ tr[1].teamSetup }}</vs-button>
+              <span class="ml-2">{{ tr[1].team && tr[1].team.shortName }}</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
   data() {
     return {
@@ -19,7 +67,40 @@ export default {
         [3,1],
         [4,5],
         [1,2]
-      ]
+      ],
+
+      selected: new Array(5)
+    }
+  },
+
+  computed: {
+    ...mapState(['teams']),
+    teamMatchups() {
+      return this.matchups.map(v => {
+        return [
+          {
+            teamSetup: v[0],
+            team: this.selected.indexOf(v[0]) !== -1? this.teams[this.selected.indexOf(v[0])] : null
+          },
+           {
+            teamSetup: v[1],
+            team: this.selected.indexOf(v[1]) !== -1? this.teams[this.selected.indexOf(v[1])] : null
+          }
+        ]
+      })
+    }
+  },
+
+  methods: {
+    checkDuplicate(value, teamIndex) {
+      // console.log(value, teamIndex)
+      this.selected[teamIndex] = value
+      this.selected.forEach((v,i) => {
+        if(i !== teamIndex && value === v) {
+          console.log(v, i)
+          this.selected[i] = null
+        }
+      })
     }
   }
 }
